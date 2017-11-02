196589780ff537c3ffa3348fc7750264e6b75e7b
alexvoez <alexvoez@gmail.com>
2017-11-02 10:16:20 -0700

    Initial Upload

---
+++ ConnectToO365.ps1
@@ -0,0 +1,5 @@
+﻿$UserCredential = Get-Credential
+Import-Module MsOnline
+Connect-MsolService -Credential $UserCredential
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $UserCredential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
